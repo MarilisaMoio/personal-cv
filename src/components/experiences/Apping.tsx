371bd172a8e57711b8f2aa559c31b9c00e3450cb
Marilisa Moio <moiomarilisa@gmail.com>
2026-07-24 16:38:51 +0200

    chore: new apping description

--- src/components/experiences/Apping.tsx
+++ src/components/experiences/Apping.tsx
@@ -5,9 +5,9 @@ import ResumeHeadings from '@/components/ResumeHeadings'
 const BoolTrainee: React.FC = () => {
     return (
         <div>
-            <ResumeHeadings title={"Jr. Web Developer"} where={"Apping s.r.l."} start={"aprile 2025"} end={"in corso"} />
+            <ResumeHeadings title={"Jr. Web Developer"} where={"Apping s.r.l."} start={"aprile 2025"} end={"luglio 2026"} />
             <p>
-                In questo ruolo mi occupo principalmente di sviluppare soluzioni custom su una delle nostre applicazioni proprietarie, Sales2APP.
+                In questo ruolo mi sono occupata principalmente dello sviluppo di soluzioni custom su Sales2APP, un CRM in PHP e MariaDB. Ho sviluppato nuove funzionalità su richiesta dei clienti, curato la manutenzione e il refactoring, lavorato sull'ottimizzazione delle performance lato applicativo e database e sviluppato gli endpoint API a supporto della nuova applicazione mobile.
             </p>
         </div>
     )
